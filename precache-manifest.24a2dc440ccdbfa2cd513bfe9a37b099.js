4be0a5df69731f056551ae89b829fc0e8f2baafe
ruizhen chi <57227399+crzlki@users.noreply.github.com>
2020-04-15 23:32:59 -0700

    Updates

---
+++ precache-manifest.24a2dc440ccdbfa2cd513bfe9a37b099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87999b4f24a0dd662871656e058c9a5a",
+    "url": "/smart-brain1/index.html"
+  },
+  {
+    "revision": "85556f30da7f1d1ab3e8",
+    "url": "/smart-brain1/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "f4c827e6b07fc012b412",
+    "url": "/smart-brain1/static/css/main.b2d886ee.chunk.css"
+  },
+  {
+    "revision": "85556f30da7f1d1ab3e8",
+    "url": "/smart-brain1/static/js/2.bd875b3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain1/static/js/2.bd875b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c827e6b07fc012b412",
+    "url": "/smart-brain1/static/js/main.117c054a.chunk.js"
+  },
+  {
+    "revision": "ea4602f11af3b9253548",
+    "url": "/smart-brain1/static/js/runtime-main.71da221f.js"
+  }
+]);
